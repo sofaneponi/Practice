45a7aeb543f0ed9fb81780b49cd1c90393ec5ecf
sofaneponi <sofaneponi@outlook.com>
2021-07-11 18:41:39 +0400

    Added list items

--- HowToExcel/Program.cs
+++ HowToExcel/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Practice
 {
@@ -12,6 +14,25 @@ namespace Practice
                 .Generate(reportData);
             
             File.WriteAllBytes("Data.xlsx", reportExcel);
+
+
+            List<Contracter> ListCont = new List<Contracter>();
+            List<DataItem> ListTask = new List<DataItem>();
+
+            ListCont.AddRange(reportData.Contracter);
+            ListTask.AddRange(reportData.Data);
+
+            Console.WriteLine("List of Contractors\n");
+            foreach (var item in ListCont)
+                if (item != null)
+                    Console.WriteLine("Contractor: {0}    \tRate Per Hour: {1}     \tAvailabality: {2}", item.Name, item.RatePerHour, item.Availabality);
+
+            Console.WriteLine("\n\nList of Tasks\n");
+            foreach (var item in ListTask)
+                if (item != null)
+                    Console.WriteLine("Task: {0,-30}       Volume: {1,-5}      \tTerm: {2}      \tBudget: {3,3}", item.Task, item.Volume, item.Term, item.Budget);
+
+            Console.ReadLine();
         }
     }
 }
